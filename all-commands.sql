f8cacbc63e71cb5db3ad845ed994ee7977c71cc8
Moises Gomez <gmoyez@outlook.com>
2023-01-19 20:55:10 -0800

    modified sql data

--- all-commands.sql
+++ all-commands.sql
@@ -42,7 +42,27 @@ INSERT INTO chemicals(cid, cname, cregistration_number, cmode_of_action, csignal
     (7,'Banner Maxx II','100-1326','FRAC 3','CAUTION','12','Target Specialties','Fungicide','Syngenta Crop Protection, LLC','YES'),
     (8,'Baythroid XL','264-840','IRAC 3A','WARNING','12','Nutrien Ag','Insecticide','Bayer CropScience','YES'),
     (9,'Biofoamer',null,null,'CAUTION','0','Nutrien Ag','Foaming Agent','BioSafe Systems','YES'),
-    (10, 'Broadform','432-1537','FRAC 11, FRAC 7','CAUTION','12','Target Speacialties','Fungicide','Bayer CropScience','YES');
+    (10,'Broadform','432-1537','FRAC 11, FRAC 7','CAUTION','12','Target Speacialties','Fungicide','Bayer CropScience','YES'),
+    (11,'CalMax',null,null,'WARNING','0','Nutrien Ag','Foliar Nutrient','Loveland Products','YES'),
+    (12,'Cascade Plus',null,null,'WARNING','0','Nutrien Ag','Surfactant','Precision Laboratories, LLC','YES'),
+    (13,'Cease','264-1155-68539','FRAC 44','CAUTION','4','Simplot','Bactericide','BioWorks, Inc','YES'),
+    (14,'Champ Formula 2 Flowable','55146-64','FRAC M01','WARNING','48','Target Specialties','Fungicide','Nufarm Americas Inc','YES'),
+    (15,'Clinch','100-894','IRAC 6','CAUTION','12','Target Specialities','Insecticide','Syngenta Crop Protection, LLC','YES'),
+    (16,'Conserve SC','62719-291','IRAC 6','CAUTION','12','Target Specialties','Insecticide','Syngenta Protection, LLC','YES'),
+    (17,'Daconil Weather Stik','50534-209-100','FRAC M05','CAUTION','12','Nutrien Ag','Fungicide','Syngenta Protection, LLC','YES'),
+    (18,'Danitol 2.4 EC','59639-35','IRAC 3A','WARNING','24','Nutrien Ag','Insecticide','Valent U.S.A LLC','YES'),
+    (19,'Decree 50 WDG','66330-35-67690','FRAC 17','CAUTION','12','Target Specialties','Fungicide','SePRO Corp','YES'),
+    (20,'Delegate WG','62719-541','IRAC 5','CAUTION','12','Nutrien Ag','Insecticide','Corteva Agriscience','YES'),
+    (21,'Di-Pel Pro DF','73049-39','IRAC 11','CAUTION','4','Target Specialties','Insecticide','Valent U.S.A LLC','YES'),
+    (22,'Dimension EC','62719-426','WSSA 3','WARNING','12','Target Specialties','Herbicide','Corteva Agriscience','YES'),
+    (23,'Drexel Captan 50 W','19713-646','FRAC M04','WARNING','12','Target Specialties','Fungicide','Drexel Chemical Company','YES'),
+    (24,'Empress','7969-355','FRAC 11','WARNING','12','Simplot','Fungicide','BASF Corp','YES'),
+    (25,'Entrust','62719-621','IRAC 5',null,'4','Nutrien Ag','Insecticide','Corteva Agriscience','YES'),
+    (26,'Evergreen Crop Protection EC 60-6','1021-1770','IRAC 3A','CAUTION','12','Target Specialties','Insecticide','Valent U.S.A LLC','YES'),
+    (27,'Flagship 25 WS','100-955','IRAC 4A','CAUTION','12','Target Specialties','Insecticide','Syngenta Crop Protection, LLC','YES'),
+    (28,'Gallery SC','62719-658','WSSA 21','CAUTION','12','Nutrien Ag','Herbicide','Corteva Agriscience','YES'),
+    (29,'Heritage SC','100-1539','FRAC 11','CAUTION','4','Simplot','Fungicide','Syngenta Crop Protection, LLC','YES'),
+    (30,'Hexygon IQ','10163-365','FRAC 11','CAUTION','4','Simplot','Ovocide','Gowan Company LLC','YES');
 
 INSERT INTO distributor(did,cdistributor_name,dphone_number,daddress,dcity,dstate,dzip) VALUES
     (1,'Target Specialties','1-800-827-4389','2478 N. Sunnyside Ave','Fresno','CA','93727'),
